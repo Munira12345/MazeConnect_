9695a70d67c67bd3e0179517df66de1d69b1186b
Munira12345 <67822795+Munira12345@users.noreply.github.com>
2026-08-17 18:51:20 +0300

    pre db

--- app/src/main/java/com/example/mazeconnect/common/SplashScreen.kt
+++ app/src/main/java/com/example/mazeconnect/common/SplashScreen.kt
@@ -28,6 +28,8 @@ import com.example.mazeconnect.ui.theme.MazeConnectTheme
 
 
 
+
+
 @Composable
 fun SplashScreen(navController: NavController) {
     val colors = listOf(Color.Black, Color(0xFF6A0DAD)) // Black to purple gradient
